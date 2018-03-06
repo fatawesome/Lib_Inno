6760243b3409fbbd54e6dc52fd122c08b0d6e44f
Gewery <35733988+Gewery@users.noreply.github.com>
2018-03-06 23:44:06 +0300

    Added 'Reserve' for user and 'Give document' for librarian

--- login/templatetags/login_extras.py
+++ login/templatetags/login_extras.py
@@ -16,7 +16,7 @@ def overdue_list(customuser_list):
 @register.filter
 def is_debtor(user):
     for rec in user.record_set.all():
-        if rec.due_to < datetime.date.today():
+        if rec.status=='o' and rec.due_to < datetime.date.today():
             return True
     return False
 
@@ -27,3 +27,11 @@ def overdue_books_list(customuser_list, user):
         if rec.due_to < datetime.date.today():
             res.append(rec.document)
     return res
+
+@register.filter
+def taken_books_list(customuser):
+    return customuser.record_set.filter(status='o')
+
+@register.filter
+def reserved_books_list(customuser):
+    return customuser.record_set.filter(status='r')
