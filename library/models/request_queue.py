bdfa5f2c699192e9a17bcb1e324692bda57fa5a4
Gewery <35733988+Gewery@users.noreply.github.com>
2018-04-01 02:41:09 +0300

    Priority sets by subtype, not by group

--- library/models/request_queue.py
+++ library/models/request_queue.py
@@ -7,14 +7,19 @@ class RequestQueueElement(models.Model):
     date = models.DateField()
     user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
     document = models.ForeignKey(Document, on_delete=models.SET_NULL, null=True)
-#    priority = models.IntegerField(default=(0 if 'Students' in [x.name for x in user.groups.all()] else 1))
     priority = models.IntegerField(default=0)
 
     def default_priority(self):
-        if 'Students' in [x.name for x in self.user.groups.all()]:
-            return 0
-        elif 'Faculty' in [x.name for x in self.user.groups.all()]:
+        if self.user.subtype == 'Students':
+            return 4
+        elif self.user.subtype == 'Instructors':
+            return 3
+        elif self.user.subtype == 'TAs':
+            return 2
+        elif self.user.subtype == 'Visiting Professors':
             return 1
+        elif self.user.subtype == 'Professors':
+            return 0
 
     # GROUP_PRIORITY = (
     #     (1, 'Faculty'),
